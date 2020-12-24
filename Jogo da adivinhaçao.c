6298e29247500e61dd2fb3b470164f3715a1babe
Dev-Alquimista <76458502+Dev-Alquimista@users.noreply.github.com>
2020-12-24 10:44:30 -0300

    Update Jogo da adivinhaçao.c

--- Jogo da adivinhaçao.c
+++ Jogo da adivinhaçao.c
@@ -3,6 +3,7 @@
 #include<time.h>
 #define TENTATIVAS 5
 #include <locale.h>
+
 int main(){
 setlocale(LC_ALL, "Portuguese");//aqui eu defino que meus printfs estarão na norma do portugues!
 
@@ -11,12 +12,13 @@ printf("*Ola seja bem-vindo ao jogo da adivinhacao*\n");
 printf("*******************************************\n");
 
 printf("voce tem %d tentativas, o numero esta entre 0 e 100 e sua pontuação também vai de 0 a 100! boa sorte :)\n", TENTATIVAS);
-
+//neste ponto funciona a geração dos numeros
 int segundos = time(0);
 srand(segundos);
 int numerogrande = rand();
 int numerosecreto = numerogrande % 100;
 
+//este "for" é o loop  que da continuidade as tentativas
 for(int i = 1; i <= TENTATIVAS; i++){
 
 int chute;
@@ -24,9 +26,10 @@ int pontos;
 
 printf("digite o seu chute: ");
 scanf("%d", &chute);
-fflush(stdin);
+fflush(stdin);//esta linha limpa o buffer do teclado
 printf("\ntentativa %d de %d\n", i, TENTATIVAS);
 
+//daqui até o final funciona as verificações do numero digitado pelo jogador
 if (chute < 0){
     printf("voce nao pode digitar numeros negativos\n");
     i--;
